ed7b89fa0ea8f59fab100fe04e6810741e7b01d0
Shrchse_ <bukanorangpremium@gmail.com>
2022-06-06 10:24:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/starwars/StarwarsViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.starwars
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.starwars.network.Characters
+import com.example.starwars.network.StarWarsApiService
+import com.example.starwars.network.Starships
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+enum class ApiStatus {LOADING, ERROR, DONE}
+
+class StarwarsViewModel:ViewModel(){
+    private val _status = MutableLiveData<ApiStatus>()
+    val status: LiveData<ApiStatus> = _status
+
+    //Api People - Character
+    private val _characters = MutableLiveData<Characters>()
+    val characters:LiveData<Characters> = _characters
+    private val _soloCharacter = MutableLiveData<Characters.Result>()
+    val soloCharacter:LiveData<Characters.Result> = _soloCharacter
+
+    // todo
+    //Api Starships - ShipStar
+    private val _starships = MutableLiveData<Starships>()
+    val starships:LiveData<Starships> = _starships
+    private val _soloStarships =MutableLiveData<Starships.Starships>()
+    val soloStarships:LiveData<Starships.Starships> = _soloStarships
+
+    fun getCharacter() {
+        viewModelScope.launch {
+            try {
+                _characters.value = StarWarsApiService.retrofitServiceApi.getCharacter()
+            } catch (e: Exception) {
+                Log.d("error", e.printStackTrace().toString())
+                _characters.value = Characters(listOf())
+            }
+        }
+    }
+        fun onCharacterClicked(characters: Characters.Result){
+            _soloCharacter.value = characters
+        }
+    fun getStarships(){
+        viewModelScope.launch {
+            try {
+                _starships.value = StarWarsApiService.retrofitServiceApi.getStarship()
+            } catch (e: Exception) {
+                Log.d("error",e.printStackTrace().toString())
+                _starships.value = Starships(listOf())
+            }
+        }
+    }
+        fun onStarshipClicked(starships: Starships.Starships){
+            _soloStarships.value = starships
+        }
+}
---
+++ app/src/main/java/com/example/starwars/network/Characters.kt
@@ -0,0 +1,12 @@
+package com.example.starwars.network
+
+data class Characters (
+    val results: List<Result>
+) {
+    data class Result(
+        val name:String,
+        val hair_color:String
+    )
+}
+
+
